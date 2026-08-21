ea1df9aec7723cfa51bd2829fa2ea1a3e26f0c9f
Lozify Developer <dev@lozify.local>
2026-08-21 10:29:09 +0800

    release: bump version to v1.2.6 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.witte.lozify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.5"
+        versionCode = 8
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
